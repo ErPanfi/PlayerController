66d1e58231848176ea2ba0ad04a90e507a435575
ErPanfi <erpanfi@gmail.com>
2014-03-18 15:09:32 +0100

    Added custom inspector for player class

---
+++ Assets/Scripts/Editor/PlayerInspector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System.Collections;
+
+[CustomPropertyDrawer(typeof(PlayerBody))]
+public class PlayerInspector : Editor 
+{
+	private PlayerBody m_body;
+	
+	public void OnEnable()
+	{
+		m_body = (PlayerBody)target;
+	} 
+
+	public override void OnInspectorGUI ()
+	{
+		serializedObject.Update();
+		
+		m_body.LinearAcceleration = EditorGUILayout.FloatField("Accelerazione Frontale", m_body.LinearAcceleration);
+		m_body.RotAcceleration = EditorGUILayout.FloatField( "Accelerazione angolare", m_body.RotAcceleration);
+		m_body.InitialJumpAcceleration = EditorGUILayout.FloatField(  "Accelerazione in salto", m_body.InitialJumpAcceleration);
+		m_body.GravityOnPlayer = EditorGUILayout.FloatField( "Gravità soggettiva", m_body.GravityOnPlayer);
+	}
+
+}
