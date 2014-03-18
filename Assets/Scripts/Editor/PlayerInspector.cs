84c009590356232af601e2d28a876f4ef65effbe
ErPanfi <erpanfi@gmail.com>
2014-03-18 16:20:52 +0100

    Extended inspector for new camera mode

--- Assets/Scripts/Editor/PlayerInspector.cs
+++ Assets/Scripts/Editor/PlayerInspector.cs
@@ -18,11 +18,15 @@ public class PlayerInspector : Editor
 		serializedObject.Update();
 		
 		Undo.RecordObject(m_body, "Player body values");
-		
+				
+		EditorGUILayout.LabelField("Settaggi fisica");
 		m_body.LinearAcceleration = EditorGUILayout.FloatField("Accelerazione Frontale", m_body.LinearAcceleration);
 		m_body.RotAcceleration = EditorGUILayout.FloatField( "Accelerazione angolare", m_body.RotAcceleration);
 		m_body.InitialJumpAcceleration = EditorGUILayout.FloatField(  "Accelerazione in salto", m_body.InitialJumpAcceleration);
 		m_body.GravityOnPlayer = EditorGUILayout.FloatField( "Gravità soggettiva", m_body.GravityOnPlayer);
+		EditorGUILayout.Space();
+		EditorGUILayout.LabelField("Settaggi camera");
+		EditorGUILayout.EnumPopup("Modalità", m_body.m_cameraMode);
 	}
 
 }
