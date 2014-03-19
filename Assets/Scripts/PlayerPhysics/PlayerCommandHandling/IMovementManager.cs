ad210fb3f403f1437a4988bca706eb976cd5af5f
ErPanfi <erpanfi@gmail.com>
2014-03-19 10:49:38 +0100

    Renamed interface file

---
+++ Assets/Scripts/PlayerPhysics/PlayerCommandHandling/IMovementManager.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IMovementManager
+{
+	void ApplyCommandOnPlayer(PlayerBody body, MovementCommand command, float deltaTime = -1);
+}
